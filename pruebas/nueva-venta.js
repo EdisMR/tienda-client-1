9d3bcef94b01483a713ee6c8413c8f961e3bf699
Edis MR <edisanthony@gmail.com>
2022-02-05 22:46:07 -0600

    pagina de prueba

--- pruebas/nueva-venta.js
+++ pruebas/nueva-venta.js
@@ -3,18 +3,26 @@ function randomProductID() {
 	return (tempData.valueOf()).toString(36)
 }
 
-let algo=new FormData()
-algo.append("idaleatorio",randomProductID())
-algo.append("nombre","nombre uno")
-algo.append("telefono","+50623454567")
-algo.append("total","250000")
-algo.append("entregado",1)/* el true tiene que ser numero */
-algo.append("notas","Esta es una nota aleatoria")
-algo.append("productos","este es un string de los productos que se compraron")
+function nuevaVenta() {
+	let algo = new FormData()
+	algo.append("idaleatorio", randomProductID())
+	algo.append("nombre", "nombre Dos")
+	algo.append("telefono", "+50612345689")
+	algo.append("total", "20000")
+	algo.append("entregado", 0) /* el true tiene que ser numero */
+	algo.append("notas", "Esta es una nota en la segunda entrada")
+	algo.append("productos", "este es un string de los productos que se compraron")
 
-fetch("http://tienda.localhost/common-scripts/new-sale.php",{
-	method:"POST",
-	body:algo
-})
-.then(e=>e.text())
-.then(e=>{console.log(e)})
+	fetch("http://tienda.localhost/common-scripts/new-sale.php", {
+			method: "POST",
+			body: algo
+		})
+		.then(e => e.text())
+		.then(e => {
+			console.log(e)
+			alertify.success("Venta realizada")
+		})
+		.catch(e=>{
+			alertify.error("No se pudo realizar la venta")
+		})
+}
